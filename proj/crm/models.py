972546d2e869c55f4d3bf5432749a54b55fa97d1
Aman Johal <ajohal21@gmail.com>
2025-06-19 13:19:34 -0700

    some models built

--- proj/crm/models.py
+++ proj/crm/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    birthdate = models.DateField()
+    email = models.CharField(max_length=200)
+    phone_number = models.IntegerField()
+    vaccines = models.ManyToManyField(Vaccine)
+
+
+class Clinician(models.Model):
+    name = models.CharField(max_length=200)
+    clients = models.ManyToManyField(User)
+
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=200)
+    date_recieved = models.DateField()
+    doses_recieved = models.IntegerField()
